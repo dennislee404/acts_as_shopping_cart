95f9c9b6f3d8367f43c57b96196fa9866a0f3ff3
Dennis Lee <dennislee404@gmail.com>
2023-11-08 13:58:56 +0800

    c

--- lib/active_record/acts/shopping_cart/collection.rb
+++ lib/active_record/acts/shopping_cart/collection.rb
@@ -13,14 +13,14 @@ module ActiveRecord
         def add(object, price, quantity = 1, cumulative = true)
           cart_item = item_for(object)
 
-          if cart_item
-            cumulative = cumulative == true ? cart_item.quantity : 0
-            cart_item.quantity = (cumulative + quantity)
-            cart_item.save
-            cart_item
-          else
+          # if cart_item
+          #   cumulative = cumulative == true ? cart_item.quantity : 0
+          #   cart_item.quantity = (cumulative + quantity)
+          #   cart_item.save
+          #   cart_item
+          # else
             shopping_cart_items.create(item: object, price: price, quantity: quantity)
-          end
+          # end
         end
 
         #
